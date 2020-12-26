c0cace7162b6bd44ffa0b0821efcc142c5d7880c
Eman Khalifa <eman.khalifa@softxpert.com>
2020-12-27 00:07:14 +0200

    my first backend response

--- first-app.js
+++ first-app.js
@@ -1,7 +1,14 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method, req.headers)
+    res.setHeader('Content-Type', 'text/html');
+    res.write(`<html>
+                <head><title>Hello World!</title></head>
+                <body>
+                    <h1>Hello Everyone! Eman is working on backend development.</h1>
+                </body>
+                </html`);
+    res.end();
 });
 
 server.listen(4200);
