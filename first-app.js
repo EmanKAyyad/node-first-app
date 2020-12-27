6f0079dab6239f69e74d958d6bcfee567c591cdb
Eman Khalifa <eman.khalifa@softxpert.com>
2020-12-27 18:25:20 +0200

    load form as homepage and get message from user input to create a file with this input on the server

--- first-app.js
+++ first-app.js
@@ -1,12 +1,47 @@
 const http = require('http');
+const fs = require("fs");
+
 
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
+
+    const url = req.url;
+    const method = req.method;
+
+    if (url === "/") {
+        res.write(`<html>
+                    <head><title>Hello World!</title></head>
+                    <body>
+                        <form action="/message-sent" method="POST">
+                            <input type="text" name="message" />
+                            <button type="submit">Submit</button>
+                        </form>
+                    </body>
+                </html`);
+        return res.end();
+    }
+    if (method === "POST" && url === "/message-sent") {
+        const body = [];
+        req.on("data", (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+
+        return req.on("end", () => {
+            const parsedBody = Buffer.concat(body).toString();
+            const message = parsedBody.split("=")[1];
+            fs.writeFileSync("message.txt", message);
+            res.statusCode = 302;
+            res.setHeader('Location', '/');
+            return res.end();
+        });
+
+    }
     res.write(`<html>
-                <head><title>Hello World!</title></head>
-                <body>
-                    <h1>Hello Everyone! Eman is working on backend development.</h1>
-                </body>
+                    <head><title>Hello World!</title></head>
+                    <body>
+                        <h1>Hello Everyone! Eman is working on backend development.</h1>
+                    </body>
                 </html`);
     res.end();
 });
