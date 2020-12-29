9a6b1507ffe14785a0027376c4cfb8b2c786bd8a
Eman Khalifa <eman.khalifa@softxpert.com>
2020-12-30 00:21:56 +0200

    separate routes handling in another file using module.exports

--- first-app.js
+++ first-app.js
@@ -1,52 +1,6 @@
 const http = require('http');
-const fs = require("fs");
+const routesHandler = require('./routes');
 
-
-const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html');
-
-    const url = req.url;
-    const method = req.method;
-
-    if (url === "/") {
-        res.write(`<html>
-                    <head><title>Hello World!</title></head>
-                    <body>
-                        <form action="/message-sent" method="POST">
-                            <input type="text" name="message" />
-                            <button type="submit">Submit</button>
-                        </form>
-                    </body>
-                </html`);
-        return res.end();
-    }
-    if (method === "POST" && url === "/message-sent") {
-        const body = [];
-        req.on("data", (chunk) => {
-            console.log(chunk);
-            body.push(chunk);
-        });
-
-        return req.on("end", () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split("=")[1];
-            fs.writeFile("message.txt", message, (err) => {
-                if (!err) {
-                    res.statusCode = 302;
-                    res.setHeader('Location', '/');
-                    return res.end();
-                }
-            });
-        });
-
-    }
-    res.write(`<html>
-                    <head><title>Hello World!</title></head>
-                    <body>
-                        <h1>Hello Everyone! Eman is working on backend development.</h1>
-                    </body>
-                </html`);
-    res.end();
-});
+const server = http.createServer(routesHandler.handler);
 
 server.listen(4200);
---
+++ routes.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+
+
+class RoutesHandler {
+
+    static handler(req, res) {
+        res.setHeader('Content-Type', 'text/html');
+
+        const url = req.url;
+        const method = req.method;
+    
+        if (url === "/") {
+            res.write(`<html>
+                        <head><title>Hello World!</title></head>
+                        <body>
+                            <form action="/message-sent" method="POST">
+                                <input type="text" name="message" />
+                                <button type="submit">Submit</button>
+                            </form>
+                        </body>
+                    </html`);
+            return res.end();
+        }
+        if (method === "POST" && url === "/message-sent") {
+            const body = [];
+            req.on("data", (chunk) => {
+                console.log(chunk);
+                body.push(chunk);
+            });
+    
+            return req.on("end", () => {
+                const parsedBody = Buffer.concat(body).toString();
+                const message = parsedBody.split("=")[1];
+                fs.writeFile("message.txt", message, (err) => {
+                    if (!err) {
+                        res.statusCode = 302;
+                        res.setHeader('Location', '/');
+                        return res.end();
+                    }
+                });
+            });
+    
+        }
+        res.write(`<html>
+                        <head><title>Hello World!</title></head>
+                        <body>
+                            <h1>Hello Everyone! Eman is working on backend development.</h1>
+                        </body>
+                    </html`);
+        res.end();
+    }
+}
+
+module.exports = RoutesHandler;
